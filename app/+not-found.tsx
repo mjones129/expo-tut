f8b77018403573f5f96551b35ab502ae87fc4a84
Matt <me@mattjones.tech>
2024-12-18 20:43:10 +0800

    add basic nav tabs

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,9 +1,11 @@
 import { View, StyleSheet } from 'react-native';
 import { Link, Stack } from 'expo-router';
+import React from 'react';
 
 export default function NotFoundScreen() {
     return(
         <>
+        
             <Stack.Screen options={{title: 'Oops! Not Found'}} />
             <View style={styles.container}>
                 <Link href="/" style={styles.button}>
